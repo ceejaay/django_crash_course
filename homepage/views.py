5db595159ec5a290a07341f38fbe73a5e3129888
Chad Jemmett <cjemmett@wehco.com>
2020-05-02 14:47:20 -0500

    13.1 added a define view method then experiemented with time.

--- homepage/views.py
+++ homepage/views.py
@@ -1,9 +1,21 @@
 from django.views.generic import TemplateView
+from datetime import timedelta
 
 
 class HomepageView(TemplateView):
     template_name = "index.html"
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        print("Context", context)
         context['my_statement'] = "Nice you see you!"
+
         return context
+    
+    def say_bye(self):
+        return "Goodbye"
+
+    def time_now(self):
+        return timedelta.max
+
+    def thing(self):
+        return "This is a thing"
