fa49da5ba273fef24229c4f96ec0e3103b3e3e32
Chad Jemmett <cjemmett@wehco.com>
2020-05-05 11:34:47 -0500

    In the process of section 27.64

---
+++ everycheese/everycheese/cheeses/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+from model_utils.models import TimeStampedModel
+from autoslug import AutoSlugField
+
+class Cheese(TimeStampedModel):
+    name = models.CharField("Name of Cheese", max_length=255)
+    slug = AutoSlugField("Cheese Address", 
+            unique=True, always_update=False, populate_from="name")
+    description = models.TextField("Description", blank=True)
+
+
+class Firmness(models.TextChoices):
+    UNSPECIFIED = "unspecified", "Unspecified"
+    SOFT = "Soft", "soft"
+    SEMI_HARD = "semi-hard", "Semi-Hard"
+    SEMI_SOFT = "semi-soft", "Semi-Soft"
+    HARD = "hard", "Hard"
